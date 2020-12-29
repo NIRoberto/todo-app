0c2d32bef1b2a90fe77674e547ec076d68023b93
Robert NIYITANGA <robertwilly668@gmail.com>
2020-12-29 17:40:46 +0200

    setuP CI

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,24 +1,20 @@
 import jwt from 'jsonwebtoken';
 
 export const auth = (req, res, next) => {
-    const token = req.header('Authorization');
-    if (!token) {
-        return res.status(401).json({
-            message:"Unauthorized"
-        })
-    }
-    try {
-        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-        req.loggedUser = verified;
+  const token = req.header('Authorization');
+  if (!token) {
+    return res.status(401).json({
+      message: 'Unauthorized',
+    });
+  }
+  try {
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.loggedUser = verified;
 
     next();
-        
-    } catch (error) {
-        return res.status(400).json({
-            Error:"Invalid token"
-        })
-        
-    }
-
-
-}
+  } catch (error) {
+    return res.status(400).json({
+      Error: 'Invalid token',
+    });
+  }
+};
--- src/routes/wrestles.js
+++ src/routes/wrestles.js
@@ -1,22 +1,20 @@
 import express from 'express';
-import { auth } from '../middleware/auth.js';
+import { auth } from '../middleware/auth';
 import {
-getWrestles,
-getOneWrestle,
-postWrestles,
-updateWrestles,
-deleteWrestles
-}  from '../controllers/wrestleNameController';
+  getWrestles,
+  getOneWrestle,
+  postWrestles,
+  updateWrestles,
+  deleteWrestles,
+} from '../controllers/wrestleNameController';
 
 const router = express.Router();
 
-router.get('/wrestles', auth,getWrestles);
-router.get('/wrestles/:id', auth,getOneWrestle);
+router.get('/wrestles', auth, getWrestles);
+router.get('/wrestles/:id', auth, getOneWrestle);
 
-router.post('/wrestles/create',auth, postWrestles);
-router.put('/wrestles/update/:id', auth,updateWrestles);
-router.delete('/wrestles/delete/:id', auth,deleteWrestles);
+router.post('/wrestles/create', auth, postWrestles);
+router.put('/wrestles/update/:id', auth, updateWrestles);
+router.delete('/wrestles/delete/:id', auth, deleteWrestles);
 
 export default router;
-
-
